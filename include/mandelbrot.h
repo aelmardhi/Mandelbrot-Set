e89796c3cfa02892d2f065a881859cf916fda845
aelmardhi <aelmardhi@gmail.com>
2022-09-09 16:47:13 +0200

    mandelbrot h

--- include/mandelbrot.h
+++ include/mandelbrot.h
@@ -1,5 +1,25 @@
+#ifndef MANDELBROT_H
+#define MANDELBROT_H
 
+#include <functional>
+#include <complex>
+
+#include "window.h"
+using Complex = std::complex<double>;
 
 class Mandelbrot{
-    Mandelbrot();
+    public:
+    Mandelbrot( Window<int> &src, Window<double> &fract, int iterMax, const std::function<Complex( Complex, Complex)> &func):src_(src), fract_(fract), iterMax_(iterMax), func_(func){}
+
+    private:
+    Window<int> &src_;
+    Window<double> &fract_;
+    int iterMax_;
+    const std::function<Complex( Complex, Complex)> &func_;
+    Complex scale(Complex c);
+    int escape(Complex c);
+    void get_number_iterations();
+    void fractal();
 };
+
+#endif
